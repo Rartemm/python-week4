0a4a240dda8bc2b43758775db4e1ce816819c038
Artem <140653244+Rartemm@users.noreply.github.com>
2023-08-11 10:18:03 +0200

    Add files via upload

---
+++ ex.1.py
@@ -0,0 +1,6 @@
+# The command below means, that the variable "some_text" is equeal to (next line)
+# whatever the user types inside. It does not matter, which type of variable will be written
+# some_text = input("Input some text: ")
+
+# After the variable is set, then it will be shown on the screen
+# print(some_text)
